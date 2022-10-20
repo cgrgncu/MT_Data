2495fd5b535175e8ced1f935452a9af82b17bb72
cgrgncu <59222358+cgrgncu@users.noreply.github.com>
2022-10-20 18:46:51 +0800

    Add files via upload

---
+++ PhaseTensor簡介/20221020_Release/yeh_export_edi_rhophspt_B_empty_600x865/Code/RUN.m
@@ -0,0 +1,8 @@
+
+clear;clc;close all
+Input_WinGLinkEdi_filename='Input_winglink_0d\TVGm03-2.edi';
+Input_title='DEMO001 Rot: 0.0 [¢X]';
+output_png_path='.\Output\';
+output_png_file_name=['yeh_export_edi_rhophspt_B_600x865_v20221019a.png'];
+ExportEdiPNG=yeh_export_edi_rhophspt_B_empty_600x865(Input_WinGLinkEdi_filename,Input_title,output_png_path,output_png_file_name);
+imshow([output_png_path,output_png_file_name])
